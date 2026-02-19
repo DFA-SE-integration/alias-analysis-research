a898efc50f0985571394760c916075898a1d5a56
dmittrey <dmitryzubahin@gmail.com>
2026-02-19 23:25:53 +0300

    tools: add sea-dsa submodule

--- scripts/01_checkout_sources.sh
+++ scripts/01_checkout_sources.sh
@@ -24,18 +24,10 @@ clone_if_missing git@github.com:secure-software-engineering/phasar.git \
   "$PHASAR_ROOT" \
   "055babd2f0a24597b9f2a9953b42dabe1fcb22ec"
 
-clone_if_missing git@github.com:seahorn/sea-dsa.git \
-  "$SDSA_ROOT" \
-  "8fc33bd29b878c8975e958da594e67ba02bb42f4"
-
 clone_if_missing git@github.com:SVF-tools/SVF.git \
   "$SVF_ROOT" \
   "197a6590bd9c695a9c3daf52622dea912ef9a002"
 
 clone_if_missing git@github.com:secure-software-engineering/PointerBench.git \
   "$PTRBENCH_ROOT" \
-  "master"
-
-clone_if_missing git@github.com:SVF-tools/Test-Suite.git \
-  "$TSUITE_ROOT" \
   "master"
